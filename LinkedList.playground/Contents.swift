9fe6e1f0ec40cd621da0f8dc90a0ae6612c29cc9
Jeet Dholakia <jeet.dholakia@peaas.co>
2020-03-19 12:13:41 +0530

    Linked List reversal
    Added linked list reversal code

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -14,7 +14,31 @@ let twoNode = Node(value: 2, next: threeNode)
 let oneNode = Node(value: 1, next: twoNode)
 
 func printList(head: Node?) {
-    print("Printing out list of nodes")
+    print("Linked list is: ")
+    var current = head
+    while current != nil {
+        print(current?.value ?? -1)
+        current = current?.next
+    }
 }
 
 printList(head: oneNode)
+
+func reverseList(head: Node?) -> Node? {
+    var currentNode = head
+    var prevNode: Node?
+    var nextNode: Node?
+    
+    while currentNode != nil {
+        nextNode = currentNode?.next
+        currentNode?.next = prevNode
+        prevNode = currentNode
+        currentNode = nextNode
+    }
+    
+    return prevNode
+}
+
+let myReversedList = reverseList(head: oneNode)
+
+printList(head: myReversedList)

330c7f31cd11991d7e01f04ac37eb19340347c6d
Jeet Dholakia <jeet.dholakia@peaas.co>
2020-03-20 16:58:05 +0530

    Linked list functions
    - Added reverse, insert and delete LL functions

--- LinkedList.playground/Contents.swift
+++ LinkedList.playground/Contents.swift
@@ -9,10 +9,6 @@ class Node {
     }
 }
 
-let threeNode = Node(value: 3, next: nil)
-let twoNode = Node(value: 2, next: threeNode)
-let oneNode = Node(value: 1, next: twoNode)
-
 func printList(head: Node?) {
     print("Linked list is: ")
     var current = head
@@ -22,23 +18,107 @@ func printList(head: Node?) {
     }
 }
 
-printList(head: oneNode)
 
-func reverseList(head: Node?) -> Node? {
-    var currentNode = head
-    var prevNode: Node?
-    var nextNode: Node?
+// Let's implement a Linked List (Insert and Delete)
+
+class LinkedList {
     
-    while currentNode != nil {
-        nextNode = currentNode?.next
-        currentNode?.next = prevNode
-        prevNode = currentNode
-        currentNode = nextNode
+    var head: Node?
+    
+    func insert() {}
+    
+    func displayList() {
+        print("Linked list is: ")
+        var current = self.head
+        while current != nil {
+            print(current?.value ?? -1)
+            current = current?.next
+        }
     }
     
-    return prevNode
+    func insert(value: Int) {
+        if head == nil {
+            head = Node(value: value, next: nil)
+        } else {
+            var currentNode = self.head
+            while currentNode?.next != nil {
+                currentNode = currentNode?.next
+            }
+            
+            currentNode?.next = Node(value: value, next: nil)
+        }
+    }
+    
+    func delete(value: Int) -> Node? {
+        if head?.value == value {
+            head = head?.next
+        }
+        
+        var currentNode = self.head
+        var previousNode: Node?
+        
+        while currentNode != nil && currentNode?.value != value {
+            previousNode = currentNode
+            currentNode = currentNode?.next
+        }
+        
+        previousNode?.next = currentNode?.next
+        return nil
+    }
+    
+    func insertInOrder(value: Int) {
+        if head == nil || head?.value ?? Int.min >= value{
+            head = Node(value: value, next: nil)
+            return
+        }
+        var currentNode = head
+        while  currentNode != nil && currentNode?.next?.value ?? Int.min < value  {
+            //currentNode?.next = Node(value: value, next: currentNode?.next)
+            currentNode = currentNode?.next
+        }
+        
+        currentNode?.next = Node(value: value, next: currentNode?.next)
+        
+    }
 }
 
-let myReversedList = reverseList(head: oneNode)
 
-printList(head: myReversedList)
+let threeNode = Node(value: 3, next: nil)
+let twoNode = Node(value: 2, next: threeNode)
+let oneNode = Node(value: 1, next: twoNode)
+
+
+let sampleList = LinkedList()
+sampleList.head = oneNode
+sampleList.insert(value: 4)
+sampleList.insert(value: 5)
+sampleList.displayList()
+sampleList.delete(value: 3)
+sampleList.displayList()
+sampleList.insertInOrder(value: 3)
+sampleList.displayList()
+
+
+/* PROBLEM SOLVING :-
+ * Reversing a linked list
+ */
+//printList(head: oneNode)
+
+//func reverseList(head: Node?) -> Node? {
+//    var currentNode = head
+//    var prevNode: Node?
+//    var nextNode: Node?
+//
+//    while currentNode != nil {
+//        nextNode = currentNode?.next
+//        currentNode?.next = prevNode
+//        prevNode = currentNode
+//        currentNode = nextNode
+//    }
+//
+//    return prevNode
+//}
+//
+//let myReversedList = reverseList(head: oneNode)
+//
+//printList(head: myReversedList)

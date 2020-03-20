b0c87b78fcb91959eaf6384ae5742e4c35c39523
Jeet Dholakia <jeet.dholakia@peaas.co>
2020-03-20 21:28:26 +0530

    Stack Class refactoring

--- Stack.playground/Contents.swift
+++ Stack.playground/Contents.swift
@@ -1,20 +1,30 @@
-public struct Stack<Element> {
-    private var storage: [Element] = []
+// Stack using Linked list
+public class Stack<Element> {
+    var top: Node<Element>?
     
-    public init() {}
-}
-
-extension Stack: CustomStringConvertible {
-
-  public var description: String {
-    let topDivider = "----top----\n"
-    let bottomDivider = "\n-----------"
-
-    let stackElements = storage
-      .map { "\($0)" }
-      .reversed()
-      .joined(separator: "\n")
-    return topDivider + stackElements + bottomDivider
-  }
+    func push(_ value: Element) {
+        let oldTop = top
+        top = Node(value: value)
+        top?.next = oldTop
+    }
+    
+    func pop() -> Element? {
+        let currentTop = top
+        top = top?.next
+        return currentTop?.value
+    }
+    
+    func peek() -> Element? {
+        return top?.value
+    }
 }
  
+// Stack using LinkedList
+class Node<Element> {
+    var value: Element
+    var next: Node?
+    
+    init(value: Element) {
+        self.value = value
+    }
+}
